5a655cbcedf9bae00e7cc3abe51ce7c1d3599abb
Samuel <samuelonyeibor21@gmail.com>
2025-06-23 15:16:26 +0100

    Implement code changes to enhance functionality and improve performance

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Icon } from "@iconify/react";
-import angularIcon from "@iconify/icons-logos/angular-icon";
+import dockerIcon from "@iconify/icons-logos/docker-icon";
 import reactIcon from "@iconify/icons-logos/react";
-import vueIcon from "@iconify/icons-logos/vue";
+import pythonIcon from "@iconify/icons-logos/python";
 
 class About extends Component {
   render() {
@@ -24,14 +24,14 @@ class About extends Component {
           <div className="row center mx-auto mb-5">
             <div className="col-md-4 mb-5 center">
               <div className="polaroid">
-                <span style={{ cursor: "auto" }}>
+                <span style={{ cursor: "auto", whiteSpace: "nowrap" }}>
                   <img
                     height="250px"
                     src={profilepic}
                     alt="Avatar placeholder"
                   />
                   <Icon
-                    icon={angularIcon}
+                    icon={dockerIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                   <Icon
@@ -39,7 +39,7 @@ class About extends Component {
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                   <Icon
-                    icon={vueIcon}
+                    icon={pythonIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                 </span>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,26 +2,41 @@ import React, { Component } from "react";
 
 class Skills extends Component {
   render() {
-    if (this.props.sharedSkills && this.props.resumeBasicInfo) {
+    if (
+      this.props.sharedSkills &&
+      this.props.sharedSkills.categories &&
+      this.props.resumeBasicInfo
+    ) {
       var sectionName = this.props.resumeBasicInfo.section_name.skills;
-      var skills = this.props.sharedSkills.icons.map(function (skills, i) {
-        return (
-          <li className="list-inline-item mx-3" key={i}>
-            <span>
-              <div className="text-center skills-tile">
-                <i className={skills.class} style={{ fontSize: "220%" }}>
-                  <p
-                    className="text-center"
-                    style={{ fontSize: "30%", marginTop: "4px" }}
-                  >
-                    {skills.name}
-                  </p>
-                </i>
-              </div>
-            </span>
-          </li>
-        );
-      });
+
+      // Map over categories
+      var categories = this.props.sharedSkills.categories.map(
+        (category, catIndex) => {
+          const skills = category.technologies.map((skill, i) => (
+            <li className="list-inline-item mx-3" key={i}>
+              <span>
+                <div className="text-center skills-tile">
+                  <i className={skill.class} style={{ fontSize: "220%" }}>
+                    <p
+                      className="text-center"
+                      style={{ fontSize: "30%", marginTop: "4px" }}
+                    >
+                      {skill.name}
+                    </p>
+                  </i>
+                </div>
+              </span>
+            </li>
+          ));
+
+          return (
+            <div key={catIndex} className="mb-4">
+              <h3 className="text-white">{category.category}</h3>
+              <ul className="list-inline mx-auto skill-icon">{skills}</ul>
+            </div>
+          );
+        }
+      );
     }
 
     return (
@@ -32,9 +47,7 @@ class Skills extends Component {
               <span className="text-white">{sectionName}</span>
             </h1>
           </div>
-          <div className="col-md-12 text-center">
-            <ul className="list-inline mx-auto skill-icon">{skills}</ul>
-          </div>
+          <div className="col-md-12 text-center">{categories}</div>
         </div>
       </section>
     );
