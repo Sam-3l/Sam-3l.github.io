f2618e953d6ded81aaab0ddd7a73d0dda267da75
Samuel <samuelonyeibor21@gmail.com>
2025-07-07 15:54:02 +0100

    Updates

---
+++ precache-manifest.9fe14e4f74743531f1c04259ac665476.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02efd1c0f38f2b01f2a01b6bdb16a7b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f85ebaed2f48aeb3f3ba",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "a7fafd41d25503695f04",
+    "url": "/static/css/main.27fb3acc.chunk.css"
+  },
+  {
+    "revision": "f85ebaed2f48aeb3f3ba",
+    "url": "/static/js/2.3fc30b06.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.3fc30b06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7fafd41d25503695f04",
+    "url": "/static/js/main.09367485.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
